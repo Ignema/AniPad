bbec4fcb2d01f6f85f42df3de76fbfee103105b0
DOUSLIMI Yasser <ignema5@hotmail.com>
2020-11-02 00:31:02 +0100

    fixed bug with service worker and changed app theme

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 if(window.location.origin != "file://"){
-    navigator.serviceWorker.register('./sw.js', { scope: '/AniPad/' })
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
     .then(function (registration)
     {
       console.log('Service worker registered successfully');
